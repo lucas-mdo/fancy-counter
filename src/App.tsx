5c92a85ecfc1770ba776fe22929d9b31e0cc10d2
lucasmdo <lucas.m.de.oliveira@accenture.com>
2024-06-04 14:03:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main>
+      <div>
+        <h1>Fancy Counter</h1>
+        <p>0</p>
+        <button>Reset</button>
+        <div>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
